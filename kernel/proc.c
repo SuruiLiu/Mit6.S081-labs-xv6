5da34aca5218013c46e1b6e79ec35d2fa9c2ede7
SuruiLiu <742901100@qq.com>
2024-09-05 21:40:41 +1000

    finish usyscall and learned pagetable skill, great!

--- kernel/proc.c
+++ kernel/proc.c
@@ -126,6 +126,14 @@ found:
     release(&p->lock);
     return 0;
   }
+  
+  //Allocate a usyscall page
+  if((p->usyscall = (struct usyscall *)kalloc()) == 0){
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+  p->usyscall->pid = p->pid;// put the pid to usyscall page
 
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
@@ -195,6 +203,14 @@ proc_pagetable(struct proc *p)
     uvmfree(pagetable, 0);
     return 0;
   }
+  
+  if(mappages(pagetable, USYSCALL, PGSIZE,
+              (uint64)(p->usyscall), PTE_R | PTE_U) < 0){
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmunmap(pagetable, TRAPFRAME, 1, 0);
+    uvmfree(pagetable, 0);
+    return 0;
+  }
 
   return pagetable;
 }
@@ -204,6 +220,7 @@ proc_pagetable(struct proc *p)
 void
 proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
+  uvmunmap(pagetable, USYSCALL, 1, 0); //release
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
   uvmfree(pagetable, sz);
